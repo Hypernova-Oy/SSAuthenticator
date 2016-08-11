828d164a5b423a8d3fac9ac32872b8acb3bd2c6c
Joonas Kylmälä <joonas.kylmala@jns.fi>
2016-08-11 14:49:10 +0000

    s/This program/Authenticator/ in license

--- lib/GPIO.pm
+++ lib/GPIO.pm
@@ -9,13 +9,13 @@ package GPIO;
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# Authenticator is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with Authenticator.  If not, see <http://www.gnu.org/licenses/>.
 
 use HiPi::Device::GPIO;
 use HiPi::Constant qw( :raspberry );
